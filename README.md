4f3a0f6cf7fe8decb5476aeb5c15d2ad8f127e79
danielevan <danielevan@gmail.com>
2018-10-29 08:33:50 -0700

    Better comments

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-# Lionize
+# Lionize - comment translation
 
-This extension translates code comments from English to Spanish, employing the [Merriam-Webster Dictionary API](https://dictionaryapi.com/).
+This extension translates code comments from English to Spanish, employing the 
+[Merriam-Webster Dictionary API](https://dictionaryapi.com/) for single words, and 
+[Google Cloud Translation API](https://cloud.google.com/translate/docs/quickstart-client-libraries#client-libraries-install-nodejs)
+for phrases. 
 
 Powered by the [Sourcegraph extension API](https://github.com/sourcegraph/sourcegraph-extension-api).
 
--- src/processing.ts
+++ src/processing.ts
@@ -29,11 +29,13 @@ export const fetchWordTranslation = (englishWord: string): Observable<number> =>
 };
 
 /**
- * Looks up the given word in the translation dictionary via the API.
+ * Looks up the given phrase in the translation service via the API.
  *
+ * @see    {@link https://cloud.google.com/nodejs/docs/reference/translate/2.1.x/|Google Cloud Translation API}
  * @param  {string} englishPhrase  A phrase to look up
  * @return {string}                The translation if known, or settings.unknownString if no translation available
  */
 
 export const fetchPhraseTranslation = (englishPhrase: string): Observable<number> => {
+  return settings.unknownString;
 }
